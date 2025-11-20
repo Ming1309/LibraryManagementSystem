b994e8c08b005062b4c772b1f3b98a81cf1b26e5
Pham Quang Minh <chinsu8426@gmail.com>
2025-11-20 23:21:46 +0700

    Day 1: Setup project

---
+++ Library.Core/Book.cs
@@ -0,0 +1,10 @@
+namespace Library.Core
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int PublishYear { get; set; }
+    }
+}
